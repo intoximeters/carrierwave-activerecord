b4a125a0db445e1311d630a141332e01917044f6
Christopher Carpenter <ccarpenter@intoxitrack.net>
2015-03-13 11:49:25 -0500

    Add back in attr_accessible
    We are still using protected attributes for now so we need this.
    
    Should be removed when we move to Strong Parameters.

--- lib/carrierwave-activerecord/storage/active_record_file.rb
+++ lib/carrierwave-activerecord/storage/active_record_file.rb
@@ -9,6 +9,11 @@ module CarrierWave
         alias_method    :delete, :destroy
         alias_attribute :read, :data
 
+        attr_accessible :identifier,
+                        :original_filename,
+                        :content_type,
+                        :size,
+                        :data
       end # ActiveRecordFile
 
     end # ActiveRecord
